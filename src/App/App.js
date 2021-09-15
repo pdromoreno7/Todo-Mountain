910a0524b2b1513401455c4d3d21eac5391aaf3c
pdromoreno7 <pdro.moreno96@gmail.com>
2021-09-15 17:21:42 -0500

    Se añadio funcion de persistencia de datos con local storage

--- src/App/App.js
+++ src/App/App.js
@@ -3,29 +3,47 @@ import { useState } from "react";
 import { AppUI } from "./AppUI";
 
 // Se creo este arreglo para mandar informacion de los todos:
-const defaultTodos = [
-  { text: "Platzi", completed: true },
-  { text: "Lavar lo platos", completed: true },
-  { text: "Church Proyect", completed: false },
-  { text: "Running", completed: false },
-  { text: "Leer Clean JavaScript", completed: false },
-];
+// const defaultTodos = [
+//   { text: "Platzi", completed: true },
+//   { text: "Lavar lo platos", completed: true },
+//   { text: "Church Proyect", completed: false },
+//   { text: "Running", completed: false },
+//   { text: "Leer Clean JavaScript", completed: false },
+// ];
 
 function App() {
-  const [todos, setTodos] = useState(defaultTodos);
+  // ==================== Manejo de local Storage ==========================================
+
+  const localStorageTodos = localStorage.getItem("TODOS_V1");
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem("TODOS_V1", JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  // ================== State ==========================================
+
+  const [todos, setTodos] = useState(parsedTodos);
   const [searchValue, setSearchValue] = useState("");
+
+  // ================== Contar todos ==================================
 
   // La siguiente linea filtra los todos completados osea los que viene con un true
   //Esto para mostraslo en todoCounter:
+
   const completedTodos = todos.filter((todo) => todo.completed === true).length;
   const totalTodos = todos.length;
 
-  // Arreglo para mostrar en panatalla los todos busccados en el search:
-  let searchedTodos = [];
-
+  // ================== Filtrado de todos por search ==================================
   //Se evalua si en estado searchValue tenga un valor
   //tranformamos a minuscula:
-  // Se llama el estado de SearchValue para pasarlo a minuscula:
+  //Se llama el estado de SearchValue para pasarlo a minuscula:
+
+  let searchedTodos = [];
+
   if (!searchValue.length >= 1) {
     searchedTodos = todos;
   } else {
@@ -37,19 +55,29 @@ function App() {
     });
   }
 
+  // ================== Funcion para persistencia de datos ==================================
+
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem("TODOS_V1", stringifiedTodos);
+    setTodos(newTodos);
+  };
+
+  // ============================ Check y dalate de todos =======================================
+
   const toggleCompleteTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
 
     const newTodos = [...todos];
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
 
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
